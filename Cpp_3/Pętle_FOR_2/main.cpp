0228ab7a01750a869ff04693d61c7428657d3ab8
danditk <danditkaczuk@gmail.com>
2020-05-28 21:44:39 +0200

    For almost reafy

--- Cpp_3/Pętle_FOR_2/main.cpp
+++ Cpp_3/Pętle_FOR_2/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <windows.h>
 
 using namespace std;
 
@@ -9,7 +10,16 @@ int main()
 {
 
     cout << "Jak    Tobie   na      imie:"; cin >> imie;
-    cout << endl << "Ile razy wypisac Twoje imie:"; cin >> ile;
+    do{
+        cout << endl << "Ile razy wypisac Twoje imie:"; cin >> ile;
+        if(ile <0){
+            cout << endl << "To musi byæ liczba dodatnia!"; cin >> ile;
+            Sleep(100);
+        } else if(ile == 0){
+            cout << endl << "Ok, dzieki za info";
+            Sleep(100);
+        }
+    }while (ile<=0);
     for (int i = 1; i<= ile; i++){
         cout << endl << i << ". " << imie;
     }
