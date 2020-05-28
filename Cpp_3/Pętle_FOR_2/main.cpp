eceb625b122053dd7812777a9bd6728ac99921ab
danditk <danditkaczuk@gmail.com>
2020-05-28 21:52:33 +0200

    For loops done

--- Cpp_3/Pętle_FOR_2/main.cpp
+++ Cpp_3/Pętle_FOR_2/main.cpp
@@ -13,13 +13,13 @@ int main()
     do{
         cout << endl << "Ile razy wypisac Twoje imie:"; cin >> ile;
         if(ile <0){
-            cout << endl << "To musi byæ liczba dodatnia!"; cin >> ile;
+            cout << endl << "To musi byæ liczba dodatnia!";
             Sleep(100);
         } else if(ile == 0){
             cout << endl << "Ok, dzieki za info";
             Sleep(100);
         }
-    }while (ile<=0);
+    }while (ile<0);
     for (int i = 1; i<= ile; i++){
         cout << endl << i << ". " << imie;
     }
