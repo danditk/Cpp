b9af8e870cdcbf4d2eaf5812b924aa91c50cd04f
danditk <danditkaczuk@gmail.com>
2020-05-30 22:03:24 +0200

    Cpp_4 generetor

--- Cpp_4/Generator_liczb_gra/main.cpp
+++ Cpp_4/Generator_liczb_gra/main.cpp
@@ -1,29 +1,43 @@
 #include <iostream>
+#include <windows.h>
 #include <cstdlib>
 #include <time.h>
 
 using namespace std;
 
-int liczba;
+int liczba, guess;
+int counting;
+int poprawna=0;
+
 
 int main()
 {
-    cout << "Hello, wybiesz liczbê od 1 - 100:" << endl;
     srand(time(NULL)); //start-randomizeing ( pobierz czas od 1970 do teraz )
-    liczba = (rand()%100)+1; // ( przekazanie wartoœci do rand i wyci¹gniêcie reszty z dzielenia przez 100 [0..99 dodanie 1 -> 1>>100] )
-    //chc¹c utworzyæ liczbê z sakresu 57..75 -> liczba=(rand()%25) + 51
-    // pierwsza liczba jest zalezna od sekund, jest cykliczna, warto przetworzyæ funkcjê, aby zakryæ tê losowoœæ
-    cout << liczba <<endl;
+    liczba = (rand()%100)+1;
+    // ( przekazanie wartoÅ“ci do rand i wyciÂ¹gniÃªcie reszty z dzielenia przez 100 [0..99 dodanie 1 -> 1>>100] )
+    //chcÂ¹c utworzyÃ¦ liczbÃª z sakresu 57..75 -> liczba=(rand()%25) + 51
+    // pierwsza liczba jest zalezna od sekund, jest cykliczna, warto przetworzyÃ¦ funkcjÃª, aby zakryÃ¦ tÃª losowoÅ“Ã¦
+    //liczba = (rand()%100)+1;
+    cout << liczba << endl;
+    cout << "Hello, wylosowalem liczbe, zgadnij jakaz to (1 - 100):";
+    while(poprawna <= 1){
+        cin >> guess;
+        if(guess==liczba){
+            poprawna=1;
+        }
+        /*if else(guess<liczba){
+            cout << "Za maÅ‚a, sprobuj ponownie.";
+            poprawna = 0;
+        } if else(guess>liczba){
+            cout << "Za duza, sprobuj ponownie.";
+            poprawna = 0;
+        } else{
+            cout << "error";
+        }*/
+        counting++;
+    }
 
-    liczba = (rand()%100)+1;
-    cout << liczba <<endl;
-    liczba = (rand()%100)+1;
-    cout << liczba <<endl;
-    liczba = (rand()%100)+1;
-    cout << liczba <<endl;
-    liczba = (rand()%100)+1;
-    cout << liczba <<endl;
-
+    cout << endl << "Gratulacje!!! Udalo Ci sie za: " << counting << "razem." << endl;
 
     return 0;
 }
