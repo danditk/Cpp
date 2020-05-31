405bc9dcaabcc9ded9e294436891a8259ac0c6aa
danditk <danditkaczuk@gmail.com>
2020-05-31 18:22:39 +0200

    Cpp_5 done

--- Cpp_5/Tablice_Fibonacci/main.cpp
+++ Cpp_5/Tablice_Fibonacci/main.cpp
@@ -1,26 +1,31 @@
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-int fib[100000]; int n, zlota;
+long double fib[100000]; int n, zlota_liczba;
 
 int main()
 {
     cout << "Ile liczb Fibonnacciego chcesz wyznaczyc: ";
     cin >> n;
 
-    fib[0]=0;
+    fib[0]=1;
     fib[1]=1;
 
     cout << "0. " << fib[0];
 
+    cout << setprecision(1000000);
+
     for(int i=2; i<n; i++){
 
         fib[i] = fib[i-1] + fib[i-2];
-        cout << endl << i << ". " << fib[i];
+       // cout << endl << i+1 << ". " << fib[i];
     }
 
-    cout << "Dzielenie ostatnich dwuch ostatnich liczb: " << zlota_liczba << endl;
+    cout << endl << n+1 << ". " << fib[n-1];
+
+    cout << endl << "Zlota liczba: " << fib[n-1]/ fib[n-2];
 
     return 0;
 }
