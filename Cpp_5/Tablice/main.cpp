18a353d1de3cebce0ec77a3366d7d4c9e76425af
danditk <danditkaczuk@gmail.com>
2020-05-31 17:16:48 +0200

    Cpp_5 - srednia ocen done

--- Cpp_5/Tablice/main.cpp
+++ Cpp_5/Tablice/main.cpp
@@ -3,21 +3,19 @@
 
 using namespace std;
 
-float oceny[5];
-string ocenki;
+float oceny[5], suma=0, srednia;
 
 int main()
 {
+    cout << "Aby obliczyc srednia..." << endl;
     for(int i=0; i<5; i++){
         cout << "Podaj ocene numer " << i+1 << " :";
         cin >> oceny[i];
+        suma += oceny[i];
     }
 
-    cout << endl;
-    for(int i=0; i<5; i++){
-        ocenki += to_string(oceny[i]) + ", ";
-    }
+    srednia = suma /5;
+    cout << endl << "Srednia ocen to: " << srednia << endl;
 
-    cout << ocenki;
     return 0;
 }
