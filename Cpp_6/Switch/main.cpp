b5809556959f46d8bd0859c475eb93a1b6434edd
danditk <danditkaczuk@gmail.com>
2020-05-31 19:25:07 +0200

    Cpp_6 ex 1 done

--- Cpp_6/Switch/main.cpp
+++ Cpp_6/Switch/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <stdio.h>
+#include <windows.h>
+#include <conio.h>
 
 using namespace std;
 
-int dzialanie=1;
+char dzialanie=1;
 float liczba1, liczba2, wynik;
 
 int main()
@@ -10,38 +13,60 @@ int main()
     cout << "Wprowadz liczbe 1: "; cin >> liczba1;
     cout << "Wprowadz liczbe 2: "; cin >> liczba2;
 
-    while(dzialanie != 0){
+    while(true){
 
+        cout << endl << endl << "MENU GLOWNE";
+        cout << endl << "-------------";
         cout << endl << "1. Dodawanie";
         cout << endl << "2. Odejmowanie";
         cout << endl << "3. Mnozenie";
         cout << endl << "4. Dzielenie";
         cout << endl << "0. Wyjscie";
-        cout << endl << "wybierz dzialanie: "; cin >> dzialanie;
+        cout << endl << "wybierz dzialanie: ";
+
+        dzialanie = getch();
 
         switch(dzialanie){
-        case 1:
-            wynik = liczba1 + liczba2;
-            cout << endl << "Wynik dodawania to: " << wynik;
-            break;
-        case 2:
-            wynik = liczba1 - liczba2;
-            cout << endl << "Wynik odejmowania to: " << wynik;
-            break;
-        case 3:
-            wynik = liczba1 * liczba2;
-            cout << endl << "Wynik mnozenia to: " << wynik;
-            break;
-        case 4:
-            wynik = liczba1 / liczba2;
-            cout << endl << "Wynik dzielenia to: " << wynik;
-            break;
-        default:
+
+        case '0':{
+            exit(0);
             break;
         }
 
-    }
+        case '1':{
+            wynik = liczba1 + liczba2;
+            cout << endl << "Wynik dodawania to: " << wynik;
+            break;
+        }
 
+        case '2':{
+            wynik = liczba1 - liczba2;
+            cout << endl << "Wynik odejmowania to: " << wynik;
+            break;
+        }
+        case '3':{
+            wynik = liczba1 * liczba2;
+            cout << endl << "Iloczyn to: " << wynik;
+            break;
+        }
+        case '4':{
+            if(liczba2 == 0) cout << endl << "Holero nie dziel przez zero!";
+            else{
+            wynik = liczba1 / liczba2;
+            cout << endl << "Iloraz to: " << wynik;
+            }
+            break;
+        }
+        default:{
+            cout << "Nie ma takiej opcji w menu!" << endl;
+            break;
+        }
+        }
+
+        getchar(); getchar();
+        system("cls");
+
+    }
 
     return 0;
 
