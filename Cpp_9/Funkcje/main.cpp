8173da62164e763131ef6a2f1ecaa21afed67b42
danditk <danditkaczuk@gmail.com>
2020-06-03 21:12:06 +0200

    Cpp_9 Added void function and feature - function argumrnt can be function f(f())

--- Cpp_9/Funkcje/main.cpp
+++ Cpp_9/Funkcje/main.cpp
@@ -7,7 +7,9 @@ float metr;
 string metrow = "metr";
 
 float ile_cali(float m); // wczytanie do kompilatora funkcji, ktora znajduje sie pod dunkcja main
-float ile_jardow(float y); // wczytanie do kompilatora funkcji, ktora znajduje sie pod dunkcja main
+float ile_jardow(float m); // wczytanie do kompilatora funkcji, ktora znajduje sie pod dunkcja main
+void ile_mil(float m);
+float ile_milki(float m);
 string zmiana_nazwy(float qty, char jednostka);  // wczytanie do kompilatora funkcji, ktora znajduje sie pod dunkcja main
 
 
@@ -17,21 +19,24 @@ int main()
 
     float cale = ile_cali(metr);
     float yard = ile_jardow(metr);
+
     string slowo_m = zmiana_nazwy(metr,'m');
     string slowo_c = zmiana_nazwy(cale,'c');
     string slowo_y = zmiana_nazwy(yard,'y');
+    string slowo_e = zmiana_nazwy(ile_milki(metr),'e'); // parametrem funkcji jak sam sie przekonalem moze byc funkcja, a nie koniecznie zmienna :D!!!
 
     cout << endl << metr << " metr" << slowo_m << " to: " << endl;
     cout << cale << " cal" << slowo_c << endl;
     cout << yard << " jard" << slowo_y << endl;
+    ile_mil(metr); cout << slowo_e << endl;
 
     return 0;
 }
 
 
-float ile_jardow(float y){
+float ile_jardow(float m){
 
-    return y * 1.0936;
+    return m * 1.0936;
     // mozna pominac wszelkie zmienne i umiescic dzialanie za funkcja return - ograniczenie ilosci kodu
 }
 
@@ -42,6 +47,18 @@ float ile_cali(float m){
 
     return(cal);
 }
+
+void ile_mil(float m){
+
+    cout << m * 0.0006213 << " mil";
+
+}
+
+float ile_milki(float m){
+
+    return m * 0.0006213;
+}
+
 
 string zmiana_nazwy(float qty, char jednostka){
 
@@ -67,6 +84,14 @@ string zmiana_nazwy(float qty, char jednostka){
         else if (qty > 1 && qty < 2) koncowka = "a";
         else if(qty>1 && qty<5) koncowka = "y";
         else koncowka = "ow";
+    }
+
+    else if (jednostka == 'e'){
+        if (qty == 0) koncowka = "";
+        else if (qty == 1) koncowka = "a";
+        else if (qty > 0 && qty < 2) koncowka = "i";
+        else if(qty>1 && qty<5) koncowka = "e";
+        else koncowka = "";
     }
 
     else koncowka = "x";
