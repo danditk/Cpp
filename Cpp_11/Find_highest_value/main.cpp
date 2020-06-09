ca6bf97fac5f21819e6e9694eb35f3b38aede274
danditk <danditkaczuk@gmail.com>
2020-06-09 20:45:16 +0200

    Cpp_ Finished, and my solution was bether :D yesgit add .! I thin as programist

--- Cpp_11/Find_highest_value/main.cpp
+++ Cpp_11/Find_highest_value/main.cpp
@@ -1,17 +1,36 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 float liczba, najwieksza;
+int ileLiczb;
+
+int a,b,c;
 
 int main()
 {
-    cout << "Podaj 3 liczby, powiem, ktora jest najwieksza: " << endl;
-    for(int i=0; i<3; i++){
+    cout << "Podaj ile liczb chesz sprawdziæ, podaj te liczby, powiem, ktora jest najwieksza" << endl;
+    cout << "Podaj ile liczb: " << endl; cin >>ileLiczb;
+    cout << "Podaj liczby: " << endl;
+    for(int i=0; i<ileLiczb; i++){
         cin >> liczba;
         if(liczba>najwieksza)najwieksza=liczba;
     }
-    cout << endl << "najwieksza liczba to: " << najwieksza;
+    cout << endl << "najwieksza liczba to: " << najwieksza << endl;
+
+
+    cout << "Podaj 3 liczby rozdzielone spacja, powiem, ktora jest najwieksza: " << endl;
+    cin >>a>>b>>c;
+
+    if(a>=b && a>=c){
+        cout << "Najwieksza liczba to: " << a;
+    }else if(b>=a && b>=c){
+        cout << "Najwieksza liczba to: " << b;
+    }else if(c>=b && c>=a){
+        cout << "Najwieksza liczba to: " << c;
+    }
+
 
     return 0;
 }
