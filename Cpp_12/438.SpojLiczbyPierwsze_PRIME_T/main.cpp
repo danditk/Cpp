efa73a7002b7dcec5ba8d247f58094d033066fad
danditk <danditkaczuk@gmail.com>
2020-06-27 00:05:09 +0200

    438.SpojLiczbyPierwsze_PRIME_T second try

--- Cpp_12/438.SpojLiczbyPierwsze_PRIME_T/main.cpp
+++ Cpp_12/438.SpojLiczbyPierwsze_PRIME_T/main.cpp
@@ -2,19 +2,25 @@
 
 using namespace std;
 
-long n=100000;
-int x=0,l;
+int x=0,qty, l;
 string text;
 
 int main()
 {
-    for(int i=1; i<n; i++){
-        cin>>l;
-        for(int k= 1; k<=l; k++){
+    cin >> qty;
+    long tab[qty];
+    for(int i=0; i<qty; i++) cin >> tab[i];
+
+    for(int i=0; i<qty; i++){
+        l = tab[i];
+        for(int k=1; k<=l; k++){
+            if(l>2 && l%2==0){
+                text="NIE";
+                break;
+            }
             for(int j=1; j<=k; j++){
-                if(k%j==0){
-                        x++;
-                }
+                if(k%j==0) x++;
+                if(x>2) break;
             }
             if(x!=2) text="NIE";
             else text="TAK";
