001b278366d758fcdd65983efe0afc4983700d76
danditk <danditkaczuk@gmail.com>
2020-07-01 21:29:54 +0200

    Cpp_12 HW - Spoj_626.Obżartuchy_GLUTTON almost done

--- Cpp_12/438.SpojLiczbyPierwsze_PRIME_T/Spoj_626.Obżartuchy_GLUTTON_HW/main.cpp
+++ Cpp_12/438.SpojLiczbyPierwsze_PRIME_T/Spoj_626.Obżartuchy_GLUTTON_HW/main.cpp
@@ -2,8 +2,22 @@
 
 using namespace std;
 
+int testQty, mObzatruchy, nCiastkaPudelko; // deklaracja ilosci testow, ilosci obzartuchow, ilosci ciastek w jednym pudelku
+int secDay = 86400; // stala ilosc sekund w dniu
+int czasObzartucha, ciastka; // deklaracja tablic o dlugosci zaleznej od pobranych zmiennych
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    cin >> testQty;
+
+    for(int i=0; i<testQty; i++){ // petla powtarzajaca sie w zaleznosci od ilosci testow, jakie maja byc przeprowadzone
+        cin >> mObzatruchy >> nCiastkaPudelko; // pobieranie wartosci od usera
+        for(int j=0; j<mObzatruchy; j++){ // petla uwzgledniajaca wszytkich obzartuchow
+            cin >> czasObzartucha; // pobranie wartosi sekundowej - czasu w ktorym obzartuch je jedno ciasteczko
+            ciastka += secDay/czasObzartucha; // obliczanie ilosci ciastek jakie podczas doby zje jeden obzartuch, sumowanie kolejnych ciastek zjedzonych przez obzartucha
+        }
+        cout << ciastka << endl;
+        ciastka=0;
+    }
     return 0;
 }
