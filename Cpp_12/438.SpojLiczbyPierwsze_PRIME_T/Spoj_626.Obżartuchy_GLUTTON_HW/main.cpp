e8b422542c3f241a20430f1cfeb69fa2dce5def6
danditk <danditkaczuk@gmail.com>
2020-07-01 21:52:54 +0200

    Cpp_12 HW - Spoj_626.Obżartuchy_GLUTTON but not accepted

--- Cpp_12/438.SpojLiczbyPierwsze_PRIME_T/Spoj_626.Obżartuchy_GLUTTON_HW/main.cpp
+++ Cpp_12/438.SpojLiczbyPierwsze_PRIME_T/Spoj_626.Obżartuchy_GLUTTON_HW/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int testQty, mObzatruchy, nCiastkaPudelko; // deklaracja ilosci testow, ilosci obzartuchow, ilosci ciastek w jednym pudelku
 int secDay = 86400; // stala ilosc sekund w dniu
-int czasObzartucha, ciastka; // deklaracja tablic o dlugosci zaleznej od pobranych zmiennych
+int czasObzartucha, ciastka, pudelka; // deklaracja tablic o dlugosci zaleznej od pobranych zmiennych
 
 int main()
 {
@@ -16,8 +16,10 @@ int main()
             cin >> czasObzartucha; // pobranie wartosi sekundowej - czasu w ktorym obzartuch je jedno ciasteczko
             ciastka += secDay/czasObzartucha; // obliczanie ilosci ciastek jakie podczas doby zje jeden obzartuch, sumowanie kolejnych ciastek zjedzonych przez obzartucha
         }
-        cout << ciastka << endl;
-        ciastka=0;
+        // obliczanie ilosci pudelek w zaleznosci od ilosci suby ciastek zjedzonych przez wszystkich obzartuchow
+        (ciastka%nCiastkaPudelko==0)? (pudelka = ciastka/nCiastkaPudelko) :(pudelka = ciastka/nCiastkaPudelko + 1); //jesli okaze sie ze liczba ciasteczek nie jest rowna tyle, co zawartosc jedngo jednego pudelka trzeba dokupic jedno pudelko, najwyzej ktos doje te ciastka :D
+        cout << pudelka;
+        ciastka=0; pudelka=0;
     }
     return 0;
 }
