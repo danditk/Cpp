88822ccb51e5fdd2e0a94da7ef4edae825557ca7
danditk <danditkaczuk@gmail.com>
2020-07-02 20:56:46 +0200

    Cpp_12 HW - Spoj_626.Obżartuchy_GLUTTON accepted :D. Answer need to have endl !

--- Cpp_12/Spoj_626.Obżartuchy_GLUTTON_HW/main.cpp
+++ Cpp_12/Spoj_626.Obżartuchy_GLUTTON_HW/main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int testQty, mObzatruchy, nCiastkaPudelko; // deklaracja ilosci testow, ilosci obzartuchow, ilosci ciastek w jednym pudelku
-int secDay = 86400; // stala ilosc sekund w dniu
-int czasObzartucha, ciastka, pudelka; // deklaracja tablic o dlugosci zaleznej od pobranych zmiennych
+double secDay = 86400; // stala ilosc sekund w dniu
+double czasObzartucha, ciastka, pudelka; // deklaracja tablic o dlugosci zaleznej od pobranych zmiennych
 
 int main()
 {
@@ -14,11 +15,12 @@ int main()
         cin >> mObzatruchy >> nCiastkaPudelko; // pobieranie wartosci od usera
         for(int j=0; j<mObzatruchy; j++){ // petla uwzgledniajaca wszytkich obzartuchow
             cin >> czasObzartucha; // pobranie wartosi sekundowej - czasu w ktorym obzartuch je jedno ciasteczko
-            ciastka += secDay/czasObzartucha; // obliczanie ilosci ciastek jakie podczas doby zje jeden obzartuch, sumowanie kolejnych ciastek zjedzonych przez obzartucha
+            ciastka += floor(secDay/czasObzartucha); // obliczanie ilosci ciastek jakie podczas doby zje jeden obzartuch, sumowanie kolejnych ciastek zjedzonych przez obzartucha
         }
         // obliczanie ilosci pudelek w zaleznosci od ilosci suby ciastek zjedzonych przez wszystkich obzartuchow
-        (ciastka%nCiastkaPudelko==0)? (pudelka = ciastka/nCiastkaPudelko) :(pudelka = ciastka/nCiastkaPudelko + 1); //jesli okaze sie ze liczba ciasteczek nie jest rowna tyle, co zawartosc jedngo jednego pudelka trzeba dokupic jedno pudelko, najwyzej ktos doje te ciastka :D
-        cout << pudelka;
+        //(ciastka%nCiastkaPudelko==0)? (pudelka = ciastka/nCiastkaPudelko) :(pudelka = ciastka/nCiastkaPudelko + 1); //jesli okaze sie ze liczba ciasteczek nie jest rowna tyle, co zawartosc jedngo jednego pudelka trzeba dokupic jedno pudelko, najwyzej ktos doje te ciastka :D
+        pudelka = ceil(ciastka/nCiastkaPudelko);
+        cout << pudelka << endl; // nie przyjmowa³ SPOJ, bo nie by³o tutaj end lajna :D
         ciastka=0; pudelka=0;
     }
     return 0;
