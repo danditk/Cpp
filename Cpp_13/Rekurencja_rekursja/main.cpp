583f3a121b145799d2b61c4854ec574477d9cedf
danditk <danditkaczuk@gmail.com>
2020-07-06 19:14:53 +0200

    Cpp_13 Rekurencje build own

--- Cpp_13/Rekurencja_rekursja/main.cpp
+++ Cpp_13/Rekurencja_rekursja/main.cpp
@@ -2,13 +2,20 @@
 
 using namespace std;
 
+/* // example 1.
 int f(int n){
     if (n==0) return 3;
     else return f(n-1)+2;
 }
+*/
+
+int potegowanie (int p, int w){
+    if (w == 0) return 1;
+    else return p * potegowanie(p,w-1);
+}
 
 int main()
 {
-    cout << f(3) << endl;
+    cout << potegowanie(2,5) << endl;
     return 0;
 }
