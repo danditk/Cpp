0c4189160f49f426fc81d4b22b3663fa68e48eff
danditk <danditkaczuk@gmail.com>
2020-07-09 22:23:35 +0200

    Cpp_14 Sortowanie quicksort - rekurencyjne

--- Cpp_14/Sortowanie. Zlozonosc algorytmow/main.cpp
+++ Cpp_14/Sortowanie. Zlozonosc algorytmow/main.cpp
@@ -20,13 +20,33 @@ void sortowanie_babelkowe(int *tab, int n){
     }
 }
 
+void sortowanie_quicksort(int *tablica, int lewy, int prawy) {
+    int v=tablica[(lewy+prawy)/2];
+    int i,j,x;
+    i=lewy;
+    j=prawy;
+    do{
+        while (tablica[i]<v) i++;
+        while (tablica[j]>v) j--;
+        if (i<=j){
+            x=tablica[i];
+            tablica[i]=tablica[j];
+            tablica[j]=x;
+            i++; j--;
+            }
+        } while (i<=j);
+    if (j>lewy) sortowanie_quicksort(tablica,lewy, j);
+    if (i<prawy) sortowanie_quicksort(tablica, i, prawy);
+    }
+
 int main()
 {
     cout << "Podaj ile liczb mamy sortowac: "; cin >> ile;
 
     //dynamiczna alokacja tablicy
-    int *tablica;
+    int *tablica, *tablica_copy;
     tablica = new int [ile];
+    tablica_copy = new int [ile];
 
     //unicjowanie generatora
     srand(time(NULL));
@@ -36,11 +56,14 @@ int main()
         tablica[i] = rand()%100000+1;
     }
 
+    for(int i=0; i<ile; i++){
+        tablica_copy[i]=tablica[i];
+    }
+
     cout << "Przed sortowaniem: ";
     for(int i=0; i<ile; i++){
         cout << tablica[i] << " ";
     } cout << endl;
-
 
     start = clock();
 
@@ -51,14 +74,28 @@ int main()
     stop = clock();
     czas = (double)(stop-start)/CLOCKS_PER_SEC;
 
+
+    cout << endl << "Czas sortowania babelkowego: " << czas << "s" << endl;
+
+    start = clock();
+
+    cout << "Sortuje teraz algorytmem rekurencyjnym quick sort, prosze czekac (ale krocej)..." << endl;
+
+    sortowanie_quicksort(tablica_copy,0,ile-1);
+
+    stop = clock();
+    czas = (double)(stop-start)/CLOCKS_PER_SEC;
+
     cout << "Po sortowaniu: ";
     for(int i=0; i<ile; i++){
         cout << tablica[i] << " ";
     } cout << endl;
 
-    delete [] tablica;
 
-    cout << endl << "Czas sortowania babelkowego: " << czas << "s" << endl;
+
+    delete [] tablica;
+    delete [] tablica_copy;
+
 
     return 0;
 }
