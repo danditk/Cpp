eca524ef3a80c2045406e965d7ca8d2d1d2cd1af
danditk <danditkaczuk@gmail.com>
2020-11-01 21:36:58 +0100

    Lerned about indicator / pointers in code

--- Cpp_10/Wskaźniki/main.cpp
+++ Cpp_10/Wskaźniki/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include <cstdlib>
+
 
 using namespace std;
 
 int ile; // alokowac pamieci na tablice
 
-int main()
-{
+int *wskaznik, zmienna;
+int zmiennaWskaznikowa;
+
+int funkcja( int *wskaznikFunkcji ){
+    *wskaznikFunkcji = 5;
+    cout << "Wskaznik zmiennej w funkcji: " << wskaznikFunkcji << " Wartosc wskaznika w funkcji: " << *wskaznikFunkcji << endl;
+}
+
+int main(){
+    /*
     cout << "Ile liczb w tablicy: ";
     cin >> ile;
 
@@ -20,6 +30,24 @@ int main()
     }
 
     delete [] tablica; // na koniec usun tablice [] o nazwie tablica, konieczne czyszczenie pamieci
+    */
+
+    // Test przekazywania zmiennych funkcji
+    zmienna = 1;
+    cout << "Zamienna adres: " << &zmienna << " Wartosc: " << zmienna << endl;
+    wskaznik = &zmienna;
+    cout << "Wskaznik zmiennej: " << wskaznik << " Wartosc wskaznika: " << *wskaznik << endl;
+    system("pause");
+
+    cout << endl << endl;
+    funkcja(wskaznik);
+    cout << "Wskaznik zmiennej po wyslaniu go funkcji: " << wskaznik << " Wartosc wskaznika po wyslaniu go funkcji: " << *wskaznik << endl;
+    system("pause");
+
+    zmiennaWskaznikowa = 100;
+    funkcja(&zmiennaWskaznikowa);
+    cout<< "zmienna wskaznikowa: " << zmiennaWskaznikowa ;
+
 
     return 0;
 }
